3aeaa58c00817ad346ea0cd37854451dfce164e9
Harrison DiAmbrosio <hdiambrosio@gmail.com>
2025-11-16 12:39:30 -0500

    create receipt, items have uuids, remove items, and edit items

--- src/app/components/register-shopper/RegisterShopper.tsx
+++ src/app/components/register-shopper/RegisterShopper.tsx
@@ -1,41 +1,41 @@
 import { useRef } from "react";
 import { ViewState } from "../lib/types";
 export default function RegisterShopper({
-  setState
+	setState
 }: {
-  setState: (state: ViewState) => void
-}) {
-  const formRef = useRef<HTMLFormElement>(null);
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const name = formRef.current?.name.value;
-    const password = formRef.current?.password.value;
-    console.log(name, password);
-    // register shopper controller logic
-  }
-  return (
-    <div>
-      <div>
-        <h1>Register Shopper</h1>
-        <form ref={formRef} onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input type="password" name="password" />
-          </label>
-          <br />
-          <button type="submit">Register</button>
-        </form>
-      </div>
-      <div>
-        <button onClick={() => setState("login-shopper")}>
-          Already have an account? Login
-        </button>
-      </div>
-    </div>
-  );
+		setState: (state: ViewState) => void
+	}) {
+	const formRef = useRef<HTMLFormElement>(null);
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		const name = formRef.current?.name.value;
+		const password = formRef.current?.password.value;
+		console.log(name, password);
+		// register shopper controller logic
+	}
+	return (
+		<div>
+			<div>
+				<h1>Register Shopper</h1>
+				<form ref={formRef} onSubmit={handleSubmit}>
+					<label>
+						Name:
+						<input type="text" name="name" />
+					</label>
+					<br />
+					<label>
+						Password:
+						<input type="password" name="password" />
+					</label>
+					<br />
+					<button type="submit">Register</button>
+				</form>
+			</div>
+			<div>
+				<button onClick={() => setState("login-shopper")}>
+					Already have an account? Login
+				</button>
+			</div>
+		</div>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 'use client';
 import { useState } from 'react';
 import { ViewState } from './components/lib/types';
+
+import Receipt from './components/receipt/Receipt';
 import RegisterShopper from './components/register-shopper/RegisterShopper';
+
 export default function Home() {
-  const [state, setState] = useState<ViewState>("register-shopper");
-  const displayComponent = (state: ViewState ) => {
-    switch (state) {
-      case "register-shopper":
-        return <RegisterShopper setState={setState} />;
-      default:
-        return <div>Invalid State</div>;
-    }
-  };
-  return (
-    <>{displayComponent(state)}</>
-  );
+	const [state, setState] = useState<ViewState>("register-shopper");
+
+	const displayComponent = (state: ViewState ) => {
+		switch (state) {
+			case "register-shopper":
+				return <>
+					<RegisterShopper setState={setState} />
+					<Receipt />
+				</>;
+			default:
+				return <div>Invalid State</div>;
+		}
+	};
+
+	return (
+		<>{displayComponent(state)}</>
+	);
 }
