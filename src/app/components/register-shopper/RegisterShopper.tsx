2a823666b5096b5782240328151fcf1a1c76a181
Harrison DiAmbrosio <hdiambrosio@gmail.com>
2025-11-20 23:08:36 -0500

    finished registering new shoppers

--- src/app/components/register-shopper/RegisterShopper.tsx
+++ src/app/components/register-shopper/RegisterShopper.tsx
@@ -1,36 +1,56 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { ViewState } from "../lib/types";
-export default function RegisterShopper({
-	setState
-}: {
-		setState: (state: ViewState) => void
-	}) {
+import { instance } from "../lib/Endpoint";
+import { register } from "next/dist/next-devtools/userspace/pages/pages-dev-overlay-setup";
+
+export default function RegisterShopper({ setState }: { setState: (state: ViewState) => void }) {
 	const formRef = useRef<HTMLFormElement>(null);
+	const [registerError, setRegisterError] = useState("");
+
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
-		const name = formRef.current?.name.value;
+		const username = formRef.current?.username.value;
 		const password = formRef.current?.password.value;
-		console.log(name, password);
-		// register shopper controller logic
+
+		const payload = {
+			"username": username,
+			"password": password
+		};
+
+		instance.post("register-shopper", payload)
+			.then((response) => {
+				console.log("Shopper-uuid: " + response.data["shopper-uuid"]);
+			})
+			.catch((error) => {
+				console.log(`Received code ${error.status} with error: ${error.response.data.message}`);
+				setRegisterError(error.response.data.message);
+			});
 	}
+
 	return (
 		<div>
 			<div>
 				<h1>Register Shopper</h1>
+
+				{registerError != "" ? <p>{registerError}</p> : <></>}
+
 				<form ref={formRef} onSubmit={handleSubmit}>
 					<label>
-						Name:
-						<input type="text" name="name" />
+						Username:
+						<input type="text" name="username" />
 					</label>
 					<br />
+
 					<label>
 						Password:
 						<input type="password" name="password" />
 					</label>
 					<br />
+
 					<button type="submit">Register</button>
 				</form>
 			</div>
+
 			<div>
 				<button onClick={() => setState("login-shopper")}>
 					Already have an account? Login
